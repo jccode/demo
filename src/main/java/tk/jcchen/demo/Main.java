4b361134f679ae2f96c9a3a911ebe2ba7e5ce9a0
jcchen <junchangchen@gmail.com>
2016-09-01 15:08:29 +0800

    init

---
+++ src/main/java/tk/jcchen/demo/Main.java
@@ -0,0 +1,16 @@
+package tk.jcchen.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by jc on 16-9-1.
+ */
+public class Main {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Hello world");
+    }
+}
